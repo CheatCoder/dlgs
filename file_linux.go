f2174675d30b295cb12b8d7a1e6799ead1c6b039
CheatCoder <CheatCoder@users.noreply.github.com>
2020-10-23 22:13:17 +0200

    SaveFile for Linux and Windows
    Add SaveFile for Linux and Windows

--- file_linux.go
+++ file_linux.go
@@ -9,8 +9,8 @@ import (
 )
 
 // File displays a file dialog, returning the selected file or directory, a bool for success, and an
-// error if it was unable to display the dialog. Filter is a string that determines 
-// which extensions should be displayed for the dialog. Separate multiple file 
+// error if it was unable to display the dialog. Filter is a string that determines
+// which extensions should be displayed for the dialog. Separate multiple file
 // extensions by spaces and use "*.extension" format for cross-platform compatibility, e.g. "*.png *.jpg".
 // A blank string for the filter will display all file types.
 func File(title, filter string, directory bool) (string, bool, error) {
@@ -46,9 +46,42 @@ func File(title, filter string, directory bool) (string, bool, error) {
 	return out, ret, err
 }
 
-// FileMulti displays a file dialog that allows for selecting multiple files. It returns the selected 
-// files, a bool for success, and an error if it was unable to display the dialog. Filter is a string 
-// that determines which files should be available for selection in the dialog. Separate multiple file 
+func SaveFile(title, filter string, directory bool) (string, bool, error) {
+	cmd, err := cmdPath()
+	if err != nil {
+		return "", false, err
+	}
+
+	dir := ""
+	if directory {
+		dir = "--directory"
+	}
+
+	fileFilter := ""
+	if filter != "" {
+		fileFilter = "--file-filter=" + filter
+	}
+
+	o, err := exec.Command(cmd, "--file-selection", "--title", title, fileFilter, dir, "--save", "--confirm-overwrite").Output()
+	if err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			ws := exitError.Sys().(syscall.WaitStatus)
+			return "", ws.ExitStatus() == 0, nil
+		}
+	}
+
+	ret := true
+	out := strings.TrimSpace(string(o))
+	if out == "" {
+		ret = false
+	}
+
+	return out, ret, err
+}
+
+// FileMulti displays a file dialog that allows for selecting multiple files. It returns the selected
+// files, a bool for success, and an error if it was unable to display the dialog. Filter is a string
+// that determines which files should be available for selection in the dialog. Separate multiple file
 // extensions by spaces and use "*.extension" format for cross-platform compatibility, e.g. "*.png *.jpg".
 // A blank string for the filter will display all file types.
 func FileMulti(title, filter string) ([]string, bool, error) {
--- file_windows.go
+++ file_windows.go
@@ -3,6 +3,7 @@
 package dlgs
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 	"syscall"
@@ -10,8 +11,8 @@ import (
 )
 
 // File displays a file dialog, returning the selected file or directory, a bool for success, and an
-// error if it was unable to display the dialog. Filter is a string that determines 
-// which extensions should be displayed for the dialog. Separate multiple file 
+// error if it was unable to display the dialog. Filter is a string that determines
+// which extensions should be displayed for the dialog. Separate multiple file
 // extensions by spaces and use "*.extension" format for cross-platform compatibility, e.g. "*.png *.jpg".
 // A blank string for the filter will display all file types.
 func File(title, filter string, directory bool) (string, bool, error) {
@@ -20,17 +21,41 @@ func File(title, filter string, directory bool) (string, bool, error) {
 		return out, ok, nil
 	}
 
-	out, ok := fileDialog(title, filter, false)
+	out, ok := fileDialog(title, filter, false, false)
 	return out, ok, nil
 }
 
-// FileMulti displays a file dialog that allows for selecting multiple files. It returns the selected 
-// files, a bool for success, and an error if it was unable to display the dialog. Filter is a string 
-// that determines which files should be available for selection in the dialog. Separate multiple file 
+func SaveFile(title, filter string) (string, bool, error) {
+	out, ok := fileDialog(title, filter, false, true)
+
+	if filter != "" {
+		filters := strings.Split(filter, " ")
+
+		var HaveExt bool
+
+		for _, v := range filters {
+			v = strings.Trim(v, "*.")
+			if strings.Contains(out, v) {
+				HaveExt = true
+				break
+			}
+		}
+
+		if !HaveExt {
+			out = fmt.Sprintf("%s.%s", out, strings.Trim(filters[0], "*."))
+		}
+	}
+
+	return out, ok, nil
+}
+
+// FileMulti displays a file dialog that allows for selecting multiple files. It returns the selected
+// files, a bool for success, and an error if it was unable to display the dialog. Filter is a string
+// that determines which files should be available for selection in the dialog. Separate multiple file
 // extensions by spaces and use "*.extension" format for cross-platform compatibility, e.g. "*.png *.jpg".
 // A blank string for the filter will display all file types.
 func FileMulti(title, filter string) ([]string, bool, error) {
-	out, ok := fileDialog(title, filter, true)
+	out, ok := fileDialog(title, filter, true, false)
 
 	files := make([]string, 0)
 
@@ -51,7 +76,7 @@ func FileMulti(title, filter string) ([]string, bool, error) {
 }
 
 // fileDialog displays file dialog.
-func fileDialog(title, filter string, multi bool) (string, bool) {
+func fileDialog(title, filter string, multi, save bool) (string, bool) {
 	var ofn openfilenameW
 	buf := make([]uint16, maxPath)
 
@@ -65,14 +90,20 @@ func fileDialog(title, filter string, multi bool) (string, bool) {
 	if filter != "" {
 		ofn.lpstrFilter = utf16PtrFromString(filter)
 	}
+	var flags int
 
-	flags := ofnExplorer | ofnFileMustExist | ofnHideReadOnly
+	if save {
+		flags = ofnExplorer | ofnHideReadOnly
+	} else {
+		flags = ofnExplorer | ofnFileMustExist | ofnHideReadOnly
+	}
 	if multi {
 		flags |= ofnAllowMultiSelect
 	}
 	ofn.flags = uint32(flags)
-
-	if getOpenFileName(&ofn) {
+	if save && getOpenFileName(&ofn) {
+		return stringFromUtf16Ptr(ofn.lpstrFile), true
+	} else if getOpenFileName(&ofn) {
 		return stringFromUtf16Ptr(ofn.lpstrFile), true
 	}
 
